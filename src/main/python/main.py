47226961acf55226a09c3b9524926084da8ce12e
johnfreyes <jhonfer96@hotmail.com>
2022-06-05 16:31:17 -0500

    add new consume service about trainning model and add results with pickle

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -1,4 +1,6 @@
 import requests
+from sklearn.linear_model import LinearRegression
+import pickle as pk
 
 URL = 'http://localhost:8090'
 
@@ -12,6 +14,17 @@ def getPersonRStats():
     print(personRStats.text)
 
 
+def getDatasetToLinearRegression():
+    Dataset = requests.get(URL + '/dataset-linear-regression')
+    X = Dataset.json()['X']
+    y = Dataset.json()['y']
+    model = LinearRegression().fit(X, y)
+    pickle_file = open('D:\\Users\\jhonf\\Documents\\Programacion\\Codigo\\Python\\ai-fuel-consumption-ratings\\src'
+                       '\\main\\resoures\\modelo.txt', 'wb')
+    pk.dump(model, pickle_file)
+
+
 if __name__ == '__main__':
-    populateDB()
-    getPersonRStats()
+    # populateDB()
+    # getPersonRStats()
+    getDatasetToLinearRegression()
